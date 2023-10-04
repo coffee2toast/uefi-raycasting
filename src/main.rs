840ccb25dcefc3a8c1ac7da3ad0a1042733bd2a6
Frederik Reiter <kontakt@frederikreiter.de>
2023-10-04 17:56:42 +0200

    Add triangle

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,62 @@
 #![no_main]
 #![no_std]
 
-use log::info;
-use uefi::{prelude::*, proto::console::text::Input, table::boot::SearchType, Identify};
+use core::slice;
+
+use tiny_skia::{Color, FillRule, Paint, PathBuilder, Transform};
+use uefi::{
+    prelude::*,
+    proto::console::{gop::GraphicsOutput, text::Input},
+    table::boot::{OpenProtocolAttributes, OpenProtocolParams, ScopedProtocol},
+};
 
 #[entry]
 fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
-    let boot_services = system_table.boot_services();
+    let bt = system_table.boot_services();
 
-    let input_handle = boot_services.get_handle_for_protocol::<Input>().unwrap();
+    let input_handle = bt.get_handle_for_protocol::<Input>().unwrap();
+    let _input = bt.open_protocol_exclusive::<Input>(input_handle).unwrap();
 
-    let mut input = boot_services
-        .open_protocol_exclusive::<Input>(input_handle)
-        .unwrap();
-
-    loop {
-        // SAFETY: This should be the only reference to the key event.
-        let input_event = unsafe { input.wait_for_key_event().unsafe_clone() };
-        boot_services
-            .wait_for_event(&mut [input_event])
-            .expect("Failed to wait for event");
-        info!("{:#?}", input.read_key().unwrap());
+    let gop_handle = bt.get_handle_for_protocol::<GraphicsOutput>().unwrap();
+    let mut gop: ScopedProtocol<GraphicsOutput> = unsafe {
+        bt.open_protocol(
+            OpenProtocolParams {
+                handle: gop_handle,
+                agent: bt.image_handle(),
+                controller: None,
+            },
+            OpenProtocolAttributes::GetProtocol,
+        )
     }
+    .unwrap();
+    let (width, height) = gop.current_mode_info().resolution();
+    let mut fb = gop.frame_buffer();
+    let slice = unsafe { slice::from_raw_parts_mut(fb.as_mut_ptr(), fb.size()) };
+    let mut pixmap = tiny_skia::PixmapMut::from_bytes(slice, width as u32, height as u32).unwrap();
+
+    let mut paint = Paint::default();
+    paint.set_color_rgba8(39, 174, 96, 255);
+    paint.anti_alias = true;
+
+    let mid_x = width as f32 / 2.0;
+    let mid_y = height as f32 / 2.0;
+
+    let mut pb = PathBuilder::new();
+    pb.move_to(-200.0, 200.0);
+    pb.line_to(200.0, 200.0);
+    pb.line_to(0.0, -200.0);
+    pb.close();
+    let path = pb.finish().unwrap();
+
+    pixmap.fill(Color::BLACK);
+    pixmap.fill_path(
+        &path,
+        &paint,
+        FillRule::Winding,
+        Transform::from_translate(mid_x, mid_y),
+        None,
+    );
+
+    loop {}
 }
